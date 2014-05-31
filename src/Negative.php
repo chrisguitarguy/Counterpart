01372a0b52b528eb81e0825beabfbe8467aa197d
Christopher Davis <cdavis9999@gmail.com>
2014-05-31 13:22:46 -0400

    Introduce the `Negative` Interface
    Allows for matchers to be aware of the fact that they may be wrapped up
    in a `LogicalNot`. If `LogicalNot` sees that it's wrapped matcher
    implements `Negative` it will call `negativeMessage` instead of doing
    it's normal "replace is with is not" routine.
    
    See #1

---
+++ src/Negative.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright 2014 Christopher Davis <http://christopherdavis.me>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package     Counterpart
+ * @copyright   2014 Christopher Davis <http://christopherdavis.me>
+ * @license     http://opensource.org/licenses/apache-2.0 Apache-2.0
+ */
+
+namespace Counterpart;
+
+/**
+ * Marks a matcher as being aware that it may be used within a `LogicalNot`. When
+ * `LogicalNot::__toString` is called, the `negativeMessage` will be called to
+ * describe the failure.
+ *
+ * @since    1.3
+ */
+interface Negative
+{
+    /**
+     * Describe the opposive of the match. If the matcher's `__toString` method
+     * returned something 'is equal to "SomeValue"' then `negativeMessage` might
+     * return something like 'is not equal to "SomeValue"'.
+     *
+     * @since   1.3
+     * @return  string
+     */
+    public function negativeMessage();
+}
