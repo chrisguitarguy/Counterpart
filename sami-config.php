89dbe54f09b0b058c87888e4995abda6773e3bf4
Christopher Davis <cdavis9999@gmail.com>
2014-06-15 20:53:16 -0400

    Add some API Documentation via Sami

---
+++ sami-config.php
@@ -0,0 +1,9 @@
+<?php
+
+return new \Sami\Sami(__DIR__.'/src', [
+    'theme'                 => 'enhanced',
+    'title'                 => 'Counterpart API',
+    'build_dir'             => __DIR__.'/sami/build',
+    'cache_dir'             => __DIR__.'/sami/cache',
+    'default_opened_level'  => 2,
+]);
