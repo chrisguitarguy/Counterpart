6b19ed5f79cef14caaa2980a89608229550b6ab8
Christopher Davis <cdavis9999@gmail.com>
2014-06-22 18:49:45 -0400

    Have Sami build from multiple versions

--- sami-config.php
+++ sami-config.php
@@ -1,9 +1,14 @@
 <?php
 
+$versions = \Sami\Version\GitVersionCollection::create(__DIR__.'/src')
+    ->addFromTags('1.*')
+    ->add('master', 'Master Branch');
+
 return new \Sami\Sami(__DIR__.'/src', [
     'theme'                 => 'enhanced',
+    'versions'              => $versions,
     'title'                 => 'Counterpart API',
-    'build_dir'             => __DIR__.'/sami/build',
-    'cache_dir'             => __DIR__.'/sami/cache',
+    'build_dir'             => __DIR__.'/sami/build/%version%',
+    'cache_dir'             => __DIR__.'/sami/cache/%version%',
     'default_opened_level'  => 2,
 ]);
